c648fcacd37e7f2e14eda694a382ba9e97d8e3e2
Oguzhan Gungor <cansingungor@gmail.com>
2013-04-14 08:09:19 +1000

    XCode Project added

---
+++ WordCounter/WordCounter/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  WordCounter
+//
+//  Created by Oguzhan Gungor on 14/04/13.
+//  Copyright (c) 2013 Oguzhan Gungor. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
